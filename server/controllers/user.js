6d3da85b6c1562c0d420c8e71636e8aa5cb28928
CongMinh3006 <trancongminh20002@gmail.com>
2024-03-05 22:50:30 +0700

    update, get, delete user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -13,6 +13,36 @@ const newUser = asyncHandle(async(req,res)=>{
     })
 })
 
+const updateUser = asyncHandle(async(req,res)=>{
+    const { mid } = req.body
+    const { name, mssv } = req.body
+    const response = await User.findByIdAndUpdate(mid, {name, mssv})
+    return res.status(200).json({
+        success: response ? true : false,
+        mes: response ? "Update user is successfully" : "Something went wrong"
+    })
+})
+
+const getAllUser = asyncHandle(async(req,res)=>{
+    const response = await User.find()
+    return res.status(200).json({
+        success: response ? true : false,
+        mes: response ? response : "Something went wrong"
+    })
+})
+
+const deleteUser = asyncHandle(async(req,res)=>{
+    const {mid} = req.body
+    const response = await User.findByIdAndDelete(mid)
+    return res.status(200).json({
+        success: response ? true : false,
+        mes: response ? "Delete user is successfully" : "Something went wrong"
+    })
+})
+
 module.exports = {
-   newUser
+   newUser,
+   updateUser,
+   getAllUser,
+   deleteUser
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,5 +2,8 @@ const router = require("express").Router()
 const ctrls = require('../controllers/user')
 
 router.post('/newUser',ctrls.newUser)
+router.get('/',ctrls.getAllUser)
+router.post('/',ctrls.updateUser)
+router.delete('/',ctrls.deleteUser)
 
 module.exports = router
